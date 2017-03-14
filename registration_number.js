077851d9017bba54f9f2ec7a8f49263254799e03
Nolonwabo <nolonwabomahanjana13@gmail.com>
2017-03-14 09:07:59 +0200

    my other changes

--- registration_number.js
+++ registration_number.js
@@ -2,14 +2,18 @@
 var ul =document.getElementById('box');
 var text = document.getElementById('regNo');
 //var addButton = document.getElementsByName('addButton');
+var regeNumber =[];
+for(i =0; i <regeNumber.length; i++){
+
+}
 function add(){
   var addButoon =document.getElementById('regNo').value;
   if(addButoon.length > 0 && list !== null){
   var list =document.createElement('li');
   list.textContent=text.value;
   ul.appendChild(list);
-
-  }
+  localStorage.setItem('reg' , addButoon);
+}
 document.getElementById('regNo').value="";
 }
-//addButoon.addEventListener ('click', add());
+console.log(localStorage);
